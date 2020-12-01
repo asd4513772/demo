1b402720a7dcd3bb3e144c93f3ea84fea751bfbe
蓝肖震 <2424611561@qq.com>
2020-12-01 11:39:28 +0800

    20201201:11.38

---
+++ src/main/resources/page/main.js
@@ -0,0 +1,20 @@
+
+ //
+ // $.post("${Student/selectAll",function(data){
+ // console.log(data);
+ // })
+
+ // (function () {
+ //         // $.post("${pageContext.request.contexPath}/Student/selectAll",function (data) {
+ //         //     console.log(data);
+ //         // })
+ //     $("#btn").click(function () {
+ //         console.log("yo")
+ //     })
+ // })
+
+ $(document).ready(function () {
+     $("#btn").click(function () {
+          console.log("yo")
+      })
+ })
